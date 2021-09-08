a6dbbc0c02a2a227237a1419da73489100fc2870
boo <nowson1a@gmail.com>
2021-09-08 18:29:14 +0300

    init

---
+++ api/resources/note.py
@@ -0,0 +1,54 @@
+from api import auth, abort, g, Resource, reqparse
+from api.models.note import NoteModel
+from api.schemas.note import note_schema, notes_schema
+
+
+class NoteResource(Resource):
+    @auth.login_required
+    def get(self, note_id):
+        author = g.user
+        note = NoteModel.query.get(note_id)
+        if not note:
+            abort(404, error=f"Note with id={note_id} not found")
+        if note.author != author:
+            abort(403, error=f"Forbidden")
+        return note, 200
+
+    @auth.login_required
+    def put(self, note_id):
+        author = g.user
+        parser = reqparse.RequestParser()
+        parser.add_argument("text", required=True)
+        parser.add_argument("private")
+        note_data = parser.parse_args()
+        note = NoteModel.query.get(note_id)
+        if not note:
+            abort(404, error=f"note {note_id} not found")
+        if note.author != author:
+            abort(403, error=f"Forbidden")
+        note.text = note_data["text"]
+        note.private = note_data.get("private")
+        note.save()
+        return note_schema.dump(note), 200
+
+    def delete(self, note_id):
+        note = NoteModel.query.get(note_id)
+        return note_schema.dump(note), 200
+
+
+class NotesListResource(Resource):
+    def get(self):
+        notes = NoteModel.query.all()
+        return notes_schema.dump(notes), 200
+
+    @auth.login_required
+    def post(self):
+        author = g.user
+        parser = reqparse.RequestParser()
+        parser.add_argument("text", required=True)
+        parser.add_argument("private")
+        note_data = parser.parse_args()
+        note = NoteModel(author_id=author.id, **note_data)
+        note.save()
+        return note_schema.dump(note), 201
+
